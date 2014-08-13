34edc4e88797acb51f906a6dfb72d98d3a038f5b
Goobs <goobs@mail.ru>
2014-08-13 11:42:12 +0400

    utf-support

--- with_asserts/context_manager.py
+++ with_asserts/context_manager.py
@@ -36,7 +36,7 @@ class AssertHTMLContext(object):
         # TODO consider validating self.response['Content-Type']
 
         # Parse the response as HTML
-        html = lxml.html.fromstring(unicode(self.response.content))
+        html = lxml.html.fromstring(self.response.content.decode('utf-8'))
         if self.selector is not None:
             # Use cssselect to filter the elements
             elements = html.cssselect(self.selector)
